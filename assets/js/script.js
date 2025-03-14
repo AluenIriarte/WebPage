7807ece514603a3606abc045fa314f705f895db3
Alan L. Perez <132615119+AluenIriarte@users.noreply.github.com>
2025-03-14 08:03:53 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Automatizar procesos", "Tableros Dinamicos", "RPA"],
+    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
