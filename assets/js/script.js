2260b6f36c0f22d686249df14c6bf6285b86c57f
Alan L. Perez <132615119+AluenIriarte@users.noreply.github.com>
2025-03-22 09:53:34 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,19 +142,28 @@ document.onkeydown = function (e) {
     }
 }
 
-// Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
-// End of Tawk.to Live Chat
 
 
+// banner //
+let index = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function moveSlide(direction) {
+    index += direction;
+    if (index < 0) index = totalSlides - 1;
+    if (index >= totalSlides) index = 0;
+    updateSlide();
+}
+
+function updateSlide() {
+    slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+// Cambio automático cada 5 segundos
+setInterval(() => moveSlide(1), 5000);
+
+//Banner Ends//
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
