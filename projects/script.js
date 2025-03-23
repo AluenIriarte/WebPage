431a57ccf92439f259e9167728c3ea1d8658c334
pruebasweb123 <asddluca@gmail.com>
2025-03-22 21:40:50 -0300

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -21,11 +21,11 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Projects | Portfolio Alan Pérez";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
+            $("#favicon").attr("href", "/assets/images/automation.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/favhand.png");
+            $("#favicon").attr("href", "/assets/images/automation.png");
         }
     });
 
